2ba04a06425cc6d05f4d2153baaca96e94d4b9e1
Enrique Ramirez <eramirez.ice@ibm.com>
2023-02-02 16:20:57 -0300

    generador de librerias

--- src/main.py
+++ src/main.py
@@ -2,14 +2,6 @@ import os
 import argparse
 import logging
 import shutil
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--name", help="Nombre de la libreria resultante", default = "LibraryCustom")
-    parser.add_argument("-lb", "--Librarys", help="Directorio donde esta ubicado el listado de librerias", default = "lib")
-    parser.add_argument("-o", "--out", help="Directorio de salida de la libreria", default = "out")
-    parser.add_argument("-dg", "--debug", help="Activacion de consola para arreglar errores", default = False)
-    args = parser.parse_args()
-    return args
 def ExistlistLib(dirlibs):
     if not os.path.exists(dirlibs):
         return False
@@ -50,11 +42,24 @@ def CreateLibSymb(pathlibs, outdir, namelibrary):
         startfilelib = listdata[:2]
         endfilelib = listdata[(len(listdata)-3):]
     datalib = startfilelib + filelib + endfilelib
+    datalib = [x for x in datalib if x!='']
     with open(os.path.join(outdir,f"{namelibrary}.lib"), "w") as file:
         datalibstr = "\n".join(datalib)
         file.write(datalibstr)
         #process file
     return True
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", help="Nombre de la libreria resultante", default = "LibraryCustom")
+    parser.add_argument("-lb", "--Librarys", help="Directorio donde esta ubicado el listado de librerias", default = "lib")
+    parser.add_argument("-o", "--out", help="Directorio de salida de la libreria", default = "out")
+    parser.add_argument("-dg", "--debug", help="Activacion de consola para arreglar errores", default = False)
+    args = parser.parse_args()
+    return args
+
 if __name__=="__main__":
     args = get_args()
-
+    if ExistlistLib(args.Librarys):
+        create_dirt(args.out, args.name)
+        CreateLibFroont(args.Librarys, args.out, args.name)
+        CreateLibSymb(args.Librarys, args.out, args.name)
